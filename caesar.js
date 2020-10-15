2461cb9c784424547015942c39861965330aada5
David <66121421+dwcoppola@users.noreply.github.com>
2020-10-15 09:08:56 -0400

    Add files via upload
    Used modulo this time instead of ridiculously complicated conditionals. Eliminated unnecessary variables and lines.

--- caesar.js
+++ caesar.js
@@ -1,40 +1,25 @@
-var alphaUpper = []
-var alphaLower = []
-var up=[]
-var lw=[]
-for (a=1;a<27;a++) {
-  alphaUpper[a] = String.fromCharCode(64 + a);
-  alphaLower[a] = String.fromCharCode(96 + a);
-};
-function caesar(input, shift) {
-  if (shift > -26 && shift < 26 && Number.isInteger(shift) == true) {
-    output = "";
-    for (i=0;i<input.length;i++) {
-      if ((alphaUpper.indexOf(input.charAt(i)) == -1) && (alphaLower.indexOf(input.charAt(i)) == -1)) {
-      output = output + input.charAt(i);
-      } else {
-        up[i] = alphaUpper.indexOf(input.charAt(i));
-        lw[i] = alphaLower.indexOf(input.charAt(i));
-        if (up[i] == -1) {
-          if ((Number(lw[i]) + shift) > 26) {
-          output = output + alphaLower[(Number(lw[i])+shift) - 26];
-          } else if ((Number(lw[i]) + shift) < 1) {
-          output = output + alphaLower[(Number(lw[i])+shift) + 26];
-          } else {
-          output = output + alphaLower[Number(lw[i])+shift];
-          };
-        } else {
-          if ((Number(up[i]) + shift) > 26) {  
-          output = output + alphaUpper[(Number(up[i])+shift) - 26];
-          } else if ((Number(up[i]) + shift) < 1) {
-          output = output + alphaUpper[(Number(up[i])+shift) + 26];
-          } else {
-          output = output + alphaUpper[Number(up[i])+shift];
-          };
-        };
-      };
-    };
-    return output;
-  };
-  return 'Hi. For 2nd parameter, please use an integer > than -26 and < 26';
+caesar = (input, shift) => {
+alphabetUppercase = []
+alphabetLowercase = []
+position = 0
+newString = ""
+	for (a=0;a<26;a++) {
+  		alphabetUppercase[a] = String.fromCharCode(65 + a);
+  		alphabetLowercase[a] = String.fromCharCode(97 + a);
+	};
+	shift %= 26
+	for (i=0;i<input.length;i++) {
+		if (alphabetUppercase.includes(input[i]) == false && alphabetLowercase.includes(input[i]) == false) {
+			newString += input[i]
+		} else if (alphabetUppercase.includes(input[i]) == true) {
+			position = alphabetUppercase.indexOf(input[i]) + shift
+				if (position > 25) {position -= 26}
+			newString += alphabetUppercase[position]
+		} else {
+			position = alphabetLowercase.indexOf(input[i]) + shift
+				if (position > 25) {position -= 26}
+			newString += alphabetLowercase[position]
+		};
+	};
+	return newString
 };
